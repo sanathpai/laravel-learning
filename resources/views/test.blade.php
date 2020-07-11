e22c5e46a283137988d14e6a8399df927ae8e444
sanath r pai <4nm17cs158@nmamit.in>
2020-07-11 17:44:41 +0530

    learnt blade

---
+++ resources/views/test.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('content')
+<h1>Hi</h1>
+
+@endsection
+@section('script')
+<h1>Bye</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::resource('users', 'UserController');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::view('/test', 'test');
